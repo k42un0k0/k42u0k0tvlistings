cf34f651a20a6f8f73db747795fa6a806a15eab2
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-01 03:38:43 +0900

    controllable days and  some

--- components/Timetable.tsx
+++ components/Timetable.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, ReactNode, useState } from "react";
+import { ReactNode, useState } from "react";
 import Head from "./Head/Head";
 import Column from "./Timetable/Column";
 import { Data, mapByCh } from "./Timetable/utils";
@@ -7,45 +7,69 @@ import { useMediaQuery } from "@material-ui/core";
 import { generateBackGround } from "../lib/utils";
 import { useRefList } from "./hooks";
 import styled from "styled-components";
+import { differenceInCalendarDays, startOfDay, format } from "date-fns";
 
 type Props = { data: Data };
 export default function Timetable({ data }: Props): JSX.Element {
-  const map = mapByCh(data.items);
+  const [map, dateArr] = mapByCh(data.items);
+  const [date, setDate] = useState(() => startOfDay(new Date()));
   const els = useRefList<HTMLDivElement>(data.items.length);
   const [index, setIndex] = useState(0);
   return (
     <Container>
+      <DateList>
+        {dateArr.map((d) => {
+          return (
+            <DateItem
+              key={d.toString()}
+              selected={differenceInCalendarDays(d, date) == 0}
+              onClick={() => {
+                setDate(d);
+              }}
+            >
+              {format(d, "M/d")}
+            </DateItem>
+          );
+        })}
+      </DateList>
       <div
         style={{
-          gridRow: "1 / 2",
-          gridColumn: "1 / 2",
+          gridRow: "2 / span 1",
+          gridColumn: "1 / span 1",
           background: generateBackGround(1),
         }}
       ></div>
       <div
         style={{
           display: "flex",
-          gridRow: "2 / 3",
-          gridColumn: "1 / 2",
+          gridRow: "3 / span 1",
+          gridColumn: "1 / span 1",
           flexDirection: "column",
-          alignItems: "flex-end",
           justifyContent: "space-between",
           background: generateBackGround(1),
         }}
       >
-        <div>0:00</div>
-        <div>24:00</div>
+        {[...Array(25).fill(null).keys()].map((i) => {
+          if (i == 24) {
+            return <div></div>;
+          }
+          return (
+            <div key={i} style={{ borderTop: "1px solid #999" }}>
+              {i.toString().padStart(2, "0")}:00
+            </div>
+          );
+        })}
       </div>
-      <SwipableFlex
+      <div
         style={{
           display: "flex",
-          gridRow: "1 / 2",
-          gridColumn: "2 / 3",
+          gridRow: "2 / span 1",
+          gridColumn: "2 / span 1",
         }}
-        index={index}
-        onChangeIndex={(index) => {
-          setIndex(index);
-        }}
+        // index={index}
+        // onChangeIndex={(index) => {
+        //   setIndex(index);
+        // }}
       >
         {[...map.keys()].map((entity, i) => {
           return (
@@ -63,42 +87,56 @@ export default function Timetable({ data }: Props): JSX.Element {
             </Head>
           );
         })}
-      </SwipableFlex>
-      <SwipableFlex
+      </div>
+      <div
         style={{
           display: "flex",
-          gridRow: "2 / 3",
-          gridColumn: "2 / 3",
+          gridRow: "3 / span 1",
+          gridColumn: "2 / span 1",
         }}
-        index={index}
-        onChangeIndex={(index) => {
-          setIndex(index);
-        }}
+        // index={index}
+        // onChangeIndex={(index) => {
+        //   setIndex(index);
+        // }}
       >
         {[...map.entries()].map((entity, i) => {
           return (
             <Column
               key={entity[0]}
+              date={date}
               ref={els.current[i]}
               entity={entity}
               indexmod12={i % 12}
             />
           );
         })}
-      </SwipableFlex>
+      </div>
     </Container>
   );
 }
 
 const Container = styled.div`
-  flex: 1;
   display: grid;
   flex-direction: column;
   grid-template-columns: auto 1fr;
-  grid-template-rows: auto 1fr;
+  grid-template-rows: auto auto 1fr;
 `;
 
-function SwipableFlex({
+const DateList = styled.ul`
+  padding-top: 20px;
+  display: flex;
+  justify-content: center;
+  grid-column: 1 / span 2;
+  grid-row: 1 / span 1;
+`;
+
+const DateItem = styled.li<{ selected: boolean }>`
+  padding: 10px;
+  cursor: pointer;
+  ${({ selected }) => (selected ? `color: blue` : ``)}
+`;
+
+function div({
   children,
   index,
   onChangeIndex,
