5708c950a7f9c8c8498c975f070b88fe8abb296e
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-01 15:39:32 +0900

    layout

--- components/Timetable/Timetable.tsx
+++ components/Timetable/Timetable.tsx
@@ -16,15 +16,10 @@ export default function Timetable(props: Props): JSX.Element {
   });
   return (
     <Container ref={containerRef}>
-      <Header
-        style={{
-          position: "sticky",
-          top: 0,
-          backgroundColor: "green",
-          display: "flex",
-        }}
-      ></Header>
-      <div style={{ height: 2000, backgroundColor: "blue" }}></div>
+      <Pad>a</Pad>
+      <DateList>a</DateList>
+      <Header>a</Header>
+      <Content>a</Content>
     </Container>
   );
 }
@@ -34,10 +29,41 @@ const Container = styled.div`
   position: sticky;
   top: 0;
   overflow-x: scroll;
-  overflow-y: hidden;
+
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-template-columns: auto 1fr;
 `;
 
 const Header = styled.header`
   position: sticky;
   top: 0;
+  grid-row: 1 / span 1;
+  grid-column: 2 / span 1;
+  background-color: #333;
+`;
+
+const Pad = styled.div`
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  grid-row: 1 / span 1;
+  grid-column: 1 / span 1;
+  background-color: #666;
+`;
+const DateList = styled.ul`
+  position: sticky;
+  left: 0;
+  grid-row: 2 / span 1;
+  grid-column: 1 / span 1;
+  background-color: #999;
+`;
+const DateListItem = styled.li``;
+const Content = styled.div`
+  grid-row: 2 / span 1;
+  grid-column: 2 / span 1;
+  background: linear-gradient(-45deg, black, white);
+  height: 2000px;
+  width: 2000px;
 `;
