460e9532b0b428311cd660fc3f5168a508812933
k42un0k0 <k42un0k0.put1put1@gmail.com>
2021-06-01 16:27:26 +0900

    pinchout

---
+++ components/Timetable/Cell.tsx
@@ -0,0 +1,110 @@
+import { differenceInMinutes, startOfDay } from "date-fns";
+import { ReactElement, useEffect, useRef, useState } from "react";
+import { ParsedItem } from "./utils";
+import Tooltip from "@material-ui/core/Tooltip";
+import { withStyles } from "@material-ui/core/styles";
+import { ClickAwayListener, useMediaQuery } from "@material-ui/core";
+import { toSpUrl } from "../../lib/rss";
+import styled from "styled-components";
+
+type Props = {
+  item: ParsedItem;
+};
+
+export default function Cell({ item }: Props): JSX.Element {
+  const [open, setOpen] = useState<boolean>(false);
+  const [mouseOver, setMouseOver] = useState<boolean>(false);
+  const ref = useRef<HTMLDivElement>();
+
+  // 親要素の大きさを単位日として自分の大きさと位置を計算する
+  useEffect(() => {
+    const minuteHeight = ref.current.parentElement.clientHeight / 24 / 60;
+    const h =
+      differenceInMinutes(item.content.end, item.content.start) * minuteHeight;
+
+    const t =
+      differenceInMinutes(item.content.start, startOfDay(item.content.start)) *
+      minuteHeight;
+    ref.current.style.height = h + "px";
+    ref.current.style.top = t + "px";
+  });
+  return (
+    <ClickAwayListener
+      onClickAway={() => {
+        setOpen(false);
+      }}
+    >
+      <Container
+        onClick={() => {
+          setOpen((state) => !state);
+        }}
+        onMouseOver={() => {
+          setMouseOver(true);
+        }}
+        onMouseLeave={() => {
+          setMouseOver(false);
+        }}
+        ref={ref}
+      >
+        <IFrameTooltip open={mouseOver || open} link={item.link}>
+          <Content>
+            {item.title}:{item.content.start.toString()}~
+            {item.content.end.toString()}
+          </Content>
+        </IFrameTooltip>
+      </Container>
+    </ClickAwayListener>
+  );
+}
+
+const Container = styled.div`
+  padding: 1px;
+  position: absolute;
+`;
+
+const Content = styled.div`
+  background-color: white;
+  height: 100%;
+  overflow: hidden;
+  -webkit-line-clamp: 1;
+`;
+
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    color: "rgba(0, 0, 0, 0.87)",
+    maxWidth: 450,
+    height: 300,
+    padding: 0,
+  },
+}))(Tooltip);
+
+type TooltipProps = {
+  link: string;
+  children: ReactElement;
+  open: boolean;
+};
+
+function IFrameTooltip({ children, link, open }: TooltipProps) {
+  const matches = useMediaQuery((theme: any) => theme.breakpoints.up("sm"));
+  return (
+    <HtmlTooltip
+      title={
+        <iframe
+          src={toSpUrl(link)}
+          style={{
+            ...(matches
+              ? { width: 450, height: 300 }
+              : { width: 300, height: 300 }),
+            zIndex: 10,
+            border: "none",
+          }}
+        />
+      }
+      placement="bottom"
+      open={open}
+      interactive
+    >
+      {children}
+    </HtmlTooltip>
+  );
+}
--- components/Timetable/Timetable.tsx
+++ components/Timetable/Timetable.tsx
@@ -1,14 +1,20 @@
 import styled from "styled-components";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Data, mapByCh } from "./utils";
+import Cell from "./Cell";
+import { differenceInCalendarDays, startOfDay } from "date-fns";
 
 type Props = {
   data: Data;
 };
 
+let scrollpercentage = 0;
 export default function Timetable({ data }: Props): JSX.Element {
   const [map, dateArr] = mapByCh(data.items);
+  const [date, setDate] = useState(() => startOfDay(new Date()));
+  const [height, setHeight] = useState(2000);
   const containerRef = useRef<HTMLDivElement>();
+  const contentRef = useRef<HTMLDivElement>();
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (containerRef.current.getBoundingClientRect().y < 0) {
@@ -17,7 +23,27 @@ export default function Timetable({ data }: Props): JSX.Element {
         containerRef.current.style.overflowY = "hidden";
       }
     });
-  });
+    contentRef.current.addEventListener(
+      "wheel",
+      (e: WheelEvent) => {
+        if (e.ctrlKey) {
+          e.preventDefault();
+          scrollpercentage =
+            containerRef.current.scrollTop / containerRef.current.scrollHeight;
+          setHeight((state) => state * (e.deltaY > 0 ? 1.05 : 1 / 1.05));
+        }
+      },
+      { passive: false }
+    );
+  }, []);
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      contentRef.current.style.height = height + "px";
+      containerRef.current.scrollTo({
+        top: containerRef.current.scrollHeight * scrollpercentage,
+      });
+    });
+  }, [height]);
   return (
     <Container ref={containerRef}>
       <Pad></Pad>
@@ -38,7 +64,27 @@ export default function Timetable({ data }: Props): JSX.Element {
           return <Head key={key}>{key}</Head>;
         })}
       </Header>
-      <Content>a</Content>
+      <Content ref={contentRef}>
+        {[...map.entries()].map(([key, items]) => {
+          return (
+            <Column key={key}>
+              {items
+                .filter((item) => {
+                  if (
+                    differenceInCalendarDays(date, item.content.end) > 0 ||
+                    differenceInCalendarDays(date, item.content.start) < 0
+                  ) {
+                    return false;
+                  }
+                  return true;
+                })
+                .map((item) => {
+                  return <Cell key={item.title + item.date} item={item} />;
+                })}
+            </Column>
+          );
+        })}
+      </Content>
     </Container>
   );
 }
@@ -48,7 +94,6 @@ const Container = styled.div`
   position: sticky;
   top: 0;
   overflow-x: scroll;
-
   display: grid;
   grid-template-rows: auto 1fr;
   grid-template-columns: auto 1fr;
@@ -60,19 +105,21 @@ const Header = styled.header`
   grid-row: 1 / span 1;
   grid-column: 2 / span 1;
   background-color: #333;
-  display: flex;
   color: white;
+  z-index: 1;
+  white-space: nowrap;
 `;
 
 const Head = styled.div`
-  flex: 1 1 auto;
+  display: inline-block;
+  width: 200px;
 `;
 
 const Pad = styled.div`
   position: sticky;
   top: 0;
   left: 0;
-  z-index: 1;
+  z-index: 2;
   grid-row: 1 / span 1;
   grid-column: 1 / span 1;
   background-color: #666;
@@ -86,6 +133,7 @@ const DateList = styled.ul`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  z-index: 1;
 `;
 const DateListItem = styled.li`
   border-top: 1px solid #ccc;
@@ -94,6 +142,12 @@ const Content = styled.div`
   grid-row: 2 / span 1;
   grid-column: 2 / span 1;
   background: linear-gradient(-45deg, black, white);
-  height: 2000px;
-  width: 2000px;
+`;
+
+const Column = styled.div`
+  border-right: 1px solid #333;
+  position: relative;
+  display: inline-block;
+  width: 200px;
+  height: 100%;
 `;
